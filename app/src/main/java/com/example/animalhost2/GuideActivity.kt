1b75d8400f56afecb9158ed3ba934bc94189d5ff
hi <gotka1128@naver.com>
2021-07-05 10:55:58 +0900

    what

---
+++ app/src/main/java/com/example/animalhost2/GuideActivity.kt
@@ -0,0 +1,11 @@
+package com.example.animalhost2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide)
+    }
+}
